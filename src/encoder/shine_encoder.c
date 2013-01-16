24f1b2ab75f86dc48f4c5400953cecebe3eba7bb
Matthias Grosser <mtgrosser@gmx.net>
2013-01-16 10:25:33 +0100

    Updating code

--- src/encoder/shine_encoder.c
+++ src/encoder/shine_encoder.c
@@ -35,8 +35,11 @@ struct shine_encoder {
 	shine_config_t shine_config;
 	shine_t	*shine;
 
-	unsigned char buffer[32768];
-	size_t buffer_length;
+	int16_t pcm_buffer[2][samp_per_frame];
+        size_t pcm_buffer_length;
+
+	unsigned char mpeg_buffer[32768];
+	size_t mpeg_buffer_length;
 };
 
 extern const struct encoder_plugin shine_encoder_plugin;
@@ -118,7 +121,7 @@ shine_encoder_setup(struct shine_encoder *encoder, GError **error)
 
 static bool
 shine_encoder_open(struct encoder *_encoder, struct audio_format *audio_format,
-		  GError **error)
+		GError **error)
 {
 	struct shine_encoder *encoder = (struct shine_encoder *)_encoder;
 
@@ -153,10 +156,84 @@ shine_encoder_close(struct encoder *_encoder)
 
 static bool
 shine_encoder_write(struct encoder *_encoder,
-		   const void *data, size_t length,
-		   G_GNUC_UNUSED GError **error)
+			const void *data, size_t length,
+			G_GNUC_UNUSED GError **error)
 {
 	struct shine_encoder *encoder = (struct shine_encoder *)_encoder;
+
+	const int16_t *src = (const int16_t*)data;
+	// int bytes_out;
+	long encoded_length;
+	unsigned int i, k;
+	unsigned char *encoded_data;
+
+	assert(encoder->mpeg_buffer_length == 0);
+
+	num_frames =
+		length / audio_format_frame_size(&encoder->audio_format); /* 4 for stereo 16 bit */
+
+	for (i = 0; i < num_frames; i++) {
+
+		/* Copy single PCM frame to working buffer */
+		for(k = 0; k < samp_per_frame; k++) {
+			encoder->pcm_buffer[0][k] = *src++;
+			encoder->pcm_buffer[1][k] = *src++;
+		}
+
+		/* Encode the frame */
+	        encoded_data = L3_encode_frame(encoder->shine, encoder->pcm_buffer, &encoded_length);
+
+		if (encoded_data == NULL) {
+			g_set_error(error, shine_encoder_quark(), 0, "Shine encoder failed");
+			return false;
+	        }
+
+		/* Copy MPEG data to buffer */
+		memcpy(&encoder->mpeg_buffer + encoder->mpeg_buffer_length, encoder->pcm_buffer, encoded_length);
+		encoder->mpeg_buffer_length += encoded_length;
+
+
+
+while (wave_get(buffer, infile, &config)) {
+    data = L3_encode_frame(s,buffer,&written);
+    write_mp3(written, data, &config);  
+  }
+
+  /* Flush and write remaining data. */
+  data = L3_flush(s,&written);
+
+
+
+twolame_encode_buffer_interleaved(encoder->options,
+						      src, num_frames,
+						      encoder->buffer,
+						      sizeof(encoder->buffer));
+	if (bytes_out < 0) {
+		g_set_error(error, twolame_encoder_quark(), 0,
+			    "twolame encoder failed");
+		return false;
+	}
+
+	encoder->buffer_length = (size_t)bytes_out;
+	return true;
+
+
+Encode audio data. Source data must have `samp_per_frames` audio samples per
+ * channels. Mono encoder only expect one channel. 
+ *
+ * Returns a pointer to freshly encoded data while `written` contains the size of
+ * available data. This pointer's memory is handled by the library and is only valid 
+ * until the next call to `L3_encode_frame` or `L3_close` and may be NULL if no data
+ * was written. */
+unsigned char *L3_encode_frame(shine_t s, int16_t data[2][samp_per_frame], long *written);
+
+/* Flush all data currently in the encoding buffer. Should be used before closing
+ * the encoder, to make all encoded data has been written. */
+unsigned char *L3_flush(shine_t s, long *written);
+
+
+
+
 
 	data = L3_encode_frame(s,buffer,&written);
 	growing_fifo_append(&encoder->buffer, data, length);
