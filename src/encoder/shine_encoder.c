b48ae3cc4607c8c9eacbae66d389e2bb899116ef
Matthias Grosser <mtgrosser@gmx.net>
2013-01-17 02:50:17 +0100

    Update code

--- src/encoder/shine_encoder.c
+++ src/encoder/shine_encoder.c
@@ -133,13 +133,29 @@ shine_encoder_close(struct encoder *_encoder)
 }
 
 static bool
+shine_encoder_flush(struct encoder *_encoder, G_GNUC_UNUSED GError **error)
+{
+	struct shine_encoder *encoder = (struct shine_encoder *)_encoder;
+
+	unsigned char *encoded_data;
+	long encoded_length;
+
+	encoded_data = L3_flush(encoder->shine, &encoded_length);
+
+	if(encoded_data != NULL)
+		shine_mpeg_buffer_push(encoder, encoded_data, encoded_length, error);
+
+	return true;
+}
+
+static bool
 shine_pcm_buffer_push(struct shine_encoder *encoder,
 			const void *data, size_t length,
 			G_GNUC_UNUSED GError **error)
 {
 	if (encoder->pcm_buffer_length + length <= sizeof(encoder->pcm_buffer)) {
 		/* append */
-		memcpy(encoder->pcm_buffer + encoder->pcm_buffer_length, data, length);
+		memcpy(&encoder->pcm_buffer + encoder->pcm_buffer_length, data, length);
 		encoder->pcm_buffer_length += length;
 		return true;
 	} else {
@@ -197,9 +213,26 @@ shine_pcm_buffer_shift(struct shine_encoder *encoder)
 		/* shift PCM buffer */
 		pcm_length = pcm_frames * audio_format_frame_size(&encoder->audio_format);
 		encoder->pcm_buffer_length -= pcm_length;
-		memmove(encoder->pcm_buffer, encoder->pcm_buffer + mpeg_frame_size, sizeof(encoder->pcm_buffer) - mpeg_frame_size);
-		return(encoder->pcm_buffer_length);
+		memmove(encoder->pcm_buffer, &encoder->pcm_buffer + mpeg_frame_size, encoder->pcm_buffer_length);
+		return encoder->pcm_buffer_length;
 	}
+}
+
+static bool
+shine_mpeg_buffer_push(struct shine_encoder *encoder,
+                        const void *data, size_t length,
+                        G_GNUC_UNUSED GError **error)
+{
+        if (encoder->mpeg_buffer_length + length <= sizeof(encoder->mpeg_buffer)) {
+                /* append */
+                memcpy(&encoder->mpeg_buffer + encoder->mpeg_buffer_length, data, length);
+                encoder->mpeg_buffer_length += length;
+                return true;
+        } else {
+                 g_set_error(error, shine_encoder_quark(), 0,
+                                    "Shine MPEG buffer overflow");
+                return false;
+        }
 }
 
 static bool
@@ -215,97 +248,35 @@ shine_encoder_write(struct encoder *_encoder,
 	unsigned num_pcm_frames;
 	unsigned int i, k;
 	unsigned char *encoded_data;
+	long encoded_length;
 
 	assert(encoder->working_buffer_length == 0);
 
-	num_pcm_frames =
-		length / audio_format_frame_size(&encoder->audio_format); /* 4 for stereo 16 bit */
-
-	// push new data to pcm buffer
-	// work off pcm buffer framewise
-	// push encoded frames to mpeg buffer
-
+	/* push new data to PCM buffer */
 	if (!shine_pcm_buffer_push(encoder, data, length, error)) {
 		L3_close(encoder->shine);
-		return(false);
-	}
-
-	while (shine_pcm_buffer_shift(encoder) > 0) {
-    data = L3_encode_frame(s,buffer,&written);
-    write_mp3(written, data, &config);
-  }
-
-
-
-
-	if (num_samples + encoder->pcm_ 
-
-	for (i = 0; i < num_frames; i++) {
-
-		/* Copy single PCM frame to working buffer */
-		for(k = 0; k < samp_per_frame; k++) {
-			encoder->pcm_buffer[0][k] = *src++;
-			encoder->pcm_buffer[1][k] = *src++;
-		}
-
-		/* Encode the frame */
-	        encoded_data = L3_encode_frame(encoder->shine, encoder->pcm_buffer, &encoded_length);
-
-		if (encoded_data == NULL) {
-			g_set_error(error, shine_encoder_quark(), 0, "Shine encoder failed");
-			return false;
-	        }
-
-		/* Copy MPEG data to buffer */
-		memcpy(&encoder->mpeg_buffer + encoder->mpeg_buffer_length, encoder->pcm_buffer, encoded_length);
-		encoder->mpeg_buffer_length += encoded_length;
-
-
-
-while (wave_get(buffer, infile, &config)) {
-    data = L3_encode_frame(s,buffer,&written);
-    write_mp3(written, data, &config);  
-  }
-
-  /* Flush and write remaining data. */
-  data = L3_flush(s,&written);
-
-
-
-twolame_encode_buffer_interleaved(encoder->options,
-						      src, num_frames,
-						      encoder->buffer,
-						      sizeof(encoder->buffer));
-	if (bytes_out < 0) {
-		g_set_error(error, twolame_encoder_quark(), 0,
-			    "twolame encoder failed");
 		return false;
 	}
 
-	encoder->buffer_length = (size_t)bytes_out;
+	/* work off PCM buffer by MPEG frame-sized chunks */
+	while (shine_pcm_buffer_shift(encoder) > 0) {
+		/* Encode the MPEG frame */
+		encoded_data = L3_encode_frame(encoder->shine, encoder->working_buffer, &encoded_length);
+
+		if (encoded_data == NULL) {
+			g_set_error(error, shine_encoder_quark(), 0, "Shine encoder failed");
+			L3_close(encoder->shine);
+			return false;
+	        }
+
+		/* Append MPEG data to buffer */
+		if (!shine_mpeg_buffer_push(encoder, encoded_data, encoded_length, error) {
+			L3_close(encoder->shine);
+			return false;
+		}
+	}
+
 	return true;
-
-
-Encode audio data. Source data must have `samp_per_frames` audio samples per
- * channels. Mono encoder only expect one channel. 
- *
- * Returns a pointer to freshly encoded data while `written` contains the size of
- * available data. This pointer's memory is handled by the library and is only valid 
- * until the next call to `L3_encode_frame` or `L3_close` and may be NULL if no data
- * was written. */
-unsigned char *L3_encode_frame(shine_t s, int16_t data[2][samp_per_frame], long *written);
-
-/* Flush all data currently in the encoding buffer. Should be used before closing
- * the encoder, to make all encoded data has been written. */
-unsigned char *L3_flush(shine_t s, long *written);
-
-
-
-
-
-	data = L3_encode_frame(s,buffer,&written);
-	growing_fifo_append(&encoder->buffer, data, length);
-	return length;
 }
 
 static size_t
@@ -313,17 +284,22 @@ shine_encoder_read(struct encoder *_encoder, void *dest, size_t length)
 {
 	struct shine_encoder *encoder = (struct shine_encoder *)_encoder;
 
-	size_t max_length;
-	const void *src = fifo_buffer_read(encoder->buffer, &max_length);
-	if (src == NULL)
-		return 0;
+	if (length > encoder->mpeg_buffer_length)
+		length = encoder->mpeg_buffer_length;
 
-	if (length > max_length)
-		length = max_length;
+	memcpy(dest, encoder->mpeg_buffer, length);
+	encoder->mpeg_buffer_length -= length;
 
-	memcpy(dest, src, length);
-	fifo_buffer_consume(encoder->buffer, length);
+	memmove(encoder->mpeg_buffer, encoder->mpeg_buffer + length,
+		encoder->mpeg_buffer_length);
+
 	return length;
+}
+
+static const char *
+shine_encoder_get_mime_type(G_GNUC_UNUSED struct encoder *_encoder)
+{
+	return "audio/mpeg";
 }
 
 const struct encoder_plugin shine_encoder_plugin = {
@@ -332,6 +308,9 @@ const struct encoder_plugin shine_encoder_plugin = {
 	.finish = shine_encoder_finish,
 	.open = shine_encoder_open,
 	.close = shine_encoder_close,
+	.end = shine_encoder_flush,
+	.flush = shine_encoder_flush,
 	.write = shine_encoder_write,
 	.read = shine_encoder_read,
+	.get_mime_type = shine_encoder_get_mime_type,
 };
